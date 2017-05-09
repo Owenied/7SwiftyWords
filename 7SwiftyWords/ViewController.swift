7ebf9fccfab52c50f4eeb0d934e9cbe2e8a570d3
Owen Duignan <owenied@me.com>
2017-05-09 11:43:07 -0500

    Built user interface for iPad style gameplay

--- 7SwiftyWords/ViewController.swift
+++ 7SwiftyWords/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+ 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitTapped(_ sender: Any) {
+    }
+    
+    @IBAction func clearTapped(_ sender: Any) {
+    }
 
 }
 
